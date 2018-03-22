7f9b56907cd85eca3b48ccee733b82810494ca73
kechxu <kechxu89@gmail.com>
2018-03-21 21:21:01 -0700

    Modify percept_transform and implement find_rocks

--- code/perception.py
+++ code/perception.py
@@ -17,6 +17,7 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # Return the binary image
     return color_select
 
+
 # Define a function to convert from image coords to rover coords
 def rover_coords(binary_img):
     # Identify nonzero pixels
@@ -38,6 +39,7 @@ def to_polar_coords(x_pixel, y_pixel):
     angles = np.arctan2(y_pixel, x_pixel)
     return dist, angles
 
+
 # Define a function to map rover space pixels to world space
 def rotate_pix(xpix, ypix, yaw):
     # Convert yaw to radians
@@ -47,6 +49,7 @@ def rotate_pix(xpix, ypix, yaw):
     ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
     # Return the result  
     return xpix_rotated, ypix_rotated
+
 
 def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
     # Apply a scaling and a translation
@@ -69,13 +72,23 @@ def pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale):
     # Return the result
     return x_pix_world, y_pix_world
 
+
 # Define a function to perform a perspective transform
 def perspect_transform(img, src, dst):
            
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
-    
-    return warped
+    mask = cv2.warpPerspective(np.ones_like(img[:, :, 0]), M, (img.shape[1], img.shape[0]))
+    return warped, mask
+
+
+def find_rocks(img, levels=(110, 110, 50)):
+    rock_pix = (img[:, :, 0] > levels[0] \
+                & img[:, :, 1] > levels[1] \
+                & img[:, :, 2] < levels[2])
+    color_select = no.zeros_like(img[:, :, 0])
+    color_select[rock_pix] = 1
+    return color_select
 
 
 # Apply the above functions in succession and update the Rover state accordingly
@@ -87,14 +100,14 @@ def perception_step(rover):
     dst_size = 5 
     bottom_offset = 6
     source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
-    destination = np.float32([[rover.img.shape[1]/2 - dst_size, rover.img.shape[0] - bottom_offset],
-                    [rover.img.shape[1]/2 + dst_size, rover.img.shape[0] - bottom_offset],
-                    [rover.img.shape[1]/2 + dst_size, rover.img.shape[0] - 2*dst_size - bottom_offset], 
-                    [rover.img.shape[1]/2 - dst_size, rover.img.shape[0] - 2*dst_size - bottom_offset],
-                  ])
+    destination = np.float32(
+        [[rover.img.shape[1] / 2 - dst_size, rover.img.shape[0] - bottom_offset],
+         [rover.img.shape[1] / 2 + dst_size, rover.img.shape[0] - bottom_offset],
+         [rover.img.shape[1] / 2 + dst_size, rover.img.shape[0] - 2 * dst_size - bottom_offset], 
+         [rover.img.shape[1] / 2 - dst_size, rover.img.shape[0] - 2 * dst_size - bottom_offset]])
 
     # 2) Apply perspective transform
-    warped = perspect_transform(rover.img, source, destination)
+    warped, mask = perspect_transform(rover.img, source, destination)
 
     # 3) Apply color threshold to identify navigable terrain/obstacles/rock samples
     thresholded = color_thresh(warped)
